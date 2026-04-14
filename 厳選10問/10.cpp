aa386e7af949558adb70efae369cec72231254a1
satoshimarumaru <2003429satoshi@gmail.com>
2026-04-14 13:12:09 +0900

    厳選問題10門終了

--- 厳選10問/10.cpp
+++ 厳選10問/10.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-#include <string>
+#include <vector>
 #include <algorithm>
 using namespace std;
-int main(){
-    
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> t(N);
+    vector<int> x(N);
+    vector<int> y(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> t[i] >> x[i] >> y[i];
+    };
+
+    int prevT = 0, prevX = 0, prevY = 0;
+    bool Can = true;
+    for (int i = 0; i < N; i++)
+    {
+        int dt = t[i] - prevT;
+        int dist = abs(x[i] - prevX) + abs(y[i] - prevY);
+
+        if (dt < dist || (dt - dist) % 2 != 0)
+        {
+            Can = false;
+            break;
+        }
+
+        prevT = t[i];
+        prevX = x[i];
+        prevY = y[i];
+    }
+    if (Can)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
